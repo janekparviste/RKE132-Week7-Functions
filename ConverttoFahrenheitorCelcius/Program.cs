ad31f1781eff2b4171d66420245abc520100c0d7
janekparviste <janek.parviste@tktk.ee>
2024-02-23 04:11:27 +0200

    RKE132-Week7-Functions

---
+++ ConverttoFahrenheitorCelcius/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Do you want to convert to Celcius or to Farenheit? Please enter (Celcius/Farenheit)");
+string userChoice = Console.ReadLine();
+
+Console.WriteLine("Provide degree: ");
+int userInput = Int32.Parse(Console.ReadLine());
+
+if (userChoice == "Celcius")
+{
+    ConverToCelsius(userInput);
+} else
+{
+    ConvertToFahrenheit(userInput);
+}
+
+static void ConverToCelsius(int argument) 
+{
+    double celsius = (argument - 32) * 5 / 9;
+    Console.WriteLine(celsius);
+}
+static void ConvertToFahrenheit(int argument) 
+{
+    double fahrenheit = (argument * 9) / 5 + 32;
+    Console.WriteLine(fahrenheit);
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Are you coming or living? (in/out)");
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+static void PrintHello() //funcion or method
+{
+    Console.WriteLine("Hello, World!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, alligator.");
+}
